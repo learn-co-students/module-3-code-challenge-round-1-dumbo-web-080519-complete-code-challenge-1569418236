68ed29c42ac83342d3acd06e60e11083fd1d263e
Josh Nho <joshik@Joshs-MacBook-Pro.local>
2019-09-25 10:35:41 -0400

    step 5 complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 3460 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,72 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const imgTag = document.querySelector("#image")
+
+  const likeButton = document.querySelector("#like_button")
+  let numLikes = document.querySelector("#likes")
+  const commentForm = document.querySelector("#comment_form")
+  const commentUl = document.querySelector("#comments")
+  
+  fetch(imageURL)
+  .then(res => res.json())
+  .then(res => {
+    const imgName = document.querySelector("#name")
+    res.comments.forEach(comment => {
+      // debugger
+      const commentInput = document.querySelector("#comment_input")
+      const commentLi = document.createElement("li")
+      const commentContent = comment.content
+      commentLi.innerText = commentContent
+      commentUl.append(commentLi)
+
+      imgName.innerText = res.name
+      imgTag.dataset.id = imageId
+      imgTag.src = res.url
+      numLikes.innerText = res.like_count
+    })
+
+    // debugger
+  })
+  
+  likeButton.addEventListener("click", function(e){
+    // debugger
+    let currentLikes = parseInt(numLikes.innerText, 10)
+    let newLikes = currentLikes + 1
+    numLikes.innerText = newLikes
+    fetch(likeURL, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId
+      })
+    })
+
+  })
+
+  commentForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    const commentInput = document.querySelector("#comment_input")
+    const commentLi = document.createElement("li")
+    const commentContent = commentInput.value
+    commentLi.innerText = commentContent
+    commentUl.append(commentLi)
+    // debugger
+
+    fetch(commentsURL, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId,
+        content: commentContent
+      })
+    })
+  })
 })
+
