7f68dbf63ea522f72ceb1ad857b170f74755653d
github username <github email address>
2019-09-25 11:06:57 -0400

    Avi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 3459 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,77 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+let imagesrc = document.getElementById("image")
+let imageName = document.getElementById("name")
+let likesElem = document.getElementById("likes")
+let commentUL = document.getElementById("comments")
+let likeButton = document.getElementById("like_button")
+let commentForm = document.getElementById("comment_form")
+let textBox = document.getElementById("comment_input")
+
+fetch(imageURL)
+.then(r => r.json())
+.then(function(data){
+  console.log(data)
+  imagesrc.src = data.url
+  imageName.innerText = data.name
+  likesElem.innerText = data.like_count
+  data.comments.forEach(function(comment){
+    commentUL.innerHTML += `
+    <li data-id= ${comment.id}>${comment.content}</li>
+    `
+    console.log(comment.id)
+  })//end of .then
+})//end of fetch image
+
+
+//end of step 1
+
+likeButton.addEventListener('click', () => {
+  numoflikes = parseInt(likesElem.innerText)
+  numoflikes += 1
+  likesElem.innerText = numoflikes
+  likedElem = parseInt(likesElem.innerText)
+
+//end of step 2
+
+
+  fetch(likeURL, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(
+      { image_id: imageId,
+        like_count: likedElem}
+    )//end of body
+  })//end of fetch for likes
+
+})// end of eventlistener for likes
+
+// end of step 3
+
+commentForm.addEventListener('submit',(e) => {
+
+  e.preventDefault()
+
+  commentUL.innerHTML += `
+  <li>${textBox.value}</li>
+  `
+  newcomm = textBox.value
+
+  //end of step 4
+
+  fetch(commentsURL, {
+    method: 'POST',
+    headers: {  'Accept': 'application/json',
+      'Content-Type': 'application/json'},
+    body: JSON.stringify(
+      { image_id: imageId,
+        content: newcomm}
+    )
+  })//end of fetch
+textBox.value = ""
+
+} )// end of comment creation
+
+// // // end of step 5
 })
